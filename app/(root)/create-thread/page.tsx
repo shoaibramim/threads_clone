6891ef4c20aa99d78cdbd5b8b9abb6e29c33974c
Shoaib <shoaibu.ramim@gmail.com>
2024-01-11 11:39:44 +0600

    First Commit
    30% of the project completed.

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,12 @@
+import {currentUser} from '@clerk/nextjs';
+import {redirect} from 'next/navigation';
+
+async function Page() {
+    const user= await currentUser();
+    if(!user) return null;
+    const userInfo= await fetchUser(user.id);
+
+    return <h1 className="head-text">Create Thread</h1>
+}
+
+export default Page;
